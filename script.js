f2a810d0ffa3caf40bb2865267ca82fbb3565e30
Shravan Murali <shravanmurali@gmail.com>
2017-11-11 21:01:58 +0530

    Added simple script

---
+++ script.js
@@ -0,0 +1,8 @@
+for(var i=2;i<12;i++)
+{var k = String(i);$('#DataGrid1__ctl'+k+'_dt1', window.parent.frames[2].document).value = 9;}
+for(var i=1;i<5;i++)
+{$('#TxtF'+String(i), window.parent.frames[2].document).value = '-';}
+$("#Button1", window.parent.frames[2].document).click();
+// for(var i=1;i<300000;i++);
+$("#Button3", window.parent.frames[2].document).click();
+// $("#Button3").click()
